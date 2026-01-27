391a9c1146f3c4758737a754d34d50ccac7db720
N.Dharaneswari <dharaneswari2004@gmail.com>
2026-01-27 19:21:03 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,12 @@
+# variables and datatypes
+a = 10
+b = 10.5
+c = "dharani"
+x = 10
+y = 20
+result = x>y
+print(result)
+print(type(result))
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
