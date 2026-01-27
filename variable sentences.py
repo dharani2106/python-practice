2be53f5db6533cd898339bcbfbdc8382abcc7fe8
N.Dharaneswari <dharaneswari2004@gmail.com>
2026-01-27 22:54:11 +0530

    Create variable sentences.py

---
+++ variable sentences.py
@@ -0,0 +1,7 @@
+# Store your name, age, and branch in variables and display them in one sentence.
+name = "N.Dharaneswari"
+degree = "B.Tech"
+branch = "Electronics and Communication Engineering"
+college_name = "Vel Tech University"
+graduating_year = 2026
+print("Good morning/afternoon.","My name is",name,".","I am currently pursuring my",degree,"in",branch,"at",college_name,",","and I will be graduating in may",graduating_year)
