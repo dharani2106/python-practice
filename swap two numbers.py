4117742d3c3977ac00e955cc1903ea3e8b189f97
N.Dharaneswari <dharaneswari2004@gmail.com>
2026-01-27 20:30:54 +0530

    Update swap two numbers.py

--- swap two numbers.py
+++ swap two numbers.py
@@ -1,8 +1,24 @@
+# swapping with temp variable
 a = 10
 b = 20
+print("before swapping:")
+print("a=", a)
+print("b=", b)
 temp = a
 a = b
 b = temp
 print("after swapping:")
 print("a=",a)
 print("b=",b)
+# swapping without temp variable
+x = 30
+y = 40
+print("before swapping:")
+print("x=", x)
+print("y=", y)
+x = x+y
+y = x-y
+x = x-y
+print("after swapping:")
+print("x=", x)
+print("y=", y)
