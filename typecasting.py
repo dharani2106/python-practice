8ad47e5c3d7a8cfa0f9143e4beeb2f6baa86d08e
N.Dharaneswari <dharaneswari2004@gmail.com>
2026-01-27 23:24:44 +0530

    Create typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,20 @@
+#conversion of int float str bool
+a = 10
+b = float(a)
+print("integer to float:",b)
+x = 23.5
+y = int(x)
+print("float to integer:",y)
+z = "45"
+p = int(z)
+print("string to integer:",p)
+r = 100.2
+s = str(r)
+print("float to string:",s)
+e = "true"
+f = bool(e)
+print("string to bool:",f)
+print(chr(67))
+print(ord('h'))
+print(bool("Hello"))
+print(bool(""))
