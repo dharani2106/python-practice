374ca90d3c97801c222a94f95db198b4510c8e76
N.Dharaneswari <dharaneswari2004@gmail.com>
2026-01-28 14:40:27 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,27 @@
+n1 = 12
+n2 = 14
+add = n1+n2
+sub = n1-n2
+mul = n1*n2
+div = n1/n2
+mod = n1%n2
+floor_div = n1//n2
+exp = n1**n2
+print("add:",add)
+print("sub:",sub)
+print("mul:",mul)
+print("div:",div)
+print("mod:",mod)
+print("floor_div:",floor_div)
+print("exp:",exp)
+num = int(input("enter the number:"))
+if num>100:
+    print("Number is greater than 100")
+else:
+    print("Number is not greater than 100")
+num = int(input("enter a number:"))
+if num >= 10 and num <= 50:
+    print("Number lies between 10 and 50:")
+else:
+    print("Number does not lies between 10 and 50:")
+
