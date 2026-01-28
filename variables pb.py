164578763f796876eb87723cd9f08a25c60dca81
N.Dharaneswari <dharaneswari2004@gmail.com>
2026-01-28 11:38:10 +0530

    Create variables pb.py

---
+++ variables pb.py
@@ -0,0 +1,80 @@
+# All variables and datatypes related problems 
+name = "Dharani"
+age = 21
+height ="5 feet 2 inches"
+degree = "b-tech"
+print(name)
+print(age)
+print(height)
+print(degree)
+a = 10
+b = 21.5
+c = "dharani"
+is_raining = False
+print(not is_raining)
+print(type(is_raining))
+print("a:", a)
+print("b:", b)
+print("c:", c)
+print(type(a))
+print(type(b))
+print(type(c))
+x = 5
+print(type(x))
+p = 34
+q = 12
+sum = p+q
+print(sum)
+res = sum
+print(type(res))
+x = 23
+y = float(x)
+print(y)
+print(type(y))
+p = 189.98
+q = int(p)
+print(q)
+print(type(q))
+a = 200
+print("before conversion :", a)
+print(type(a))
+b = str(a)
+print("after conversion:", b)
+print(type(b))
+c = "13"
+d = (int(c) + 10)
+print(d)
+print(type(d))
+x = 32
+y = 45
+z = y
+y = x
+x = z
+print(x)
+print(y)
+x = 1
+y = 2
+x = x+y
+y = x-y
+x = x-y
+print(x)
+print(y)
+a = 10
+b = 40
+c = 60
+sum = a+b+c
+avg = sum/3
+res = avg
+print(res)
+print(type(res))
+n = 20
+res = n>100
+print(res)
+x = 0
+print(bool(x))
+y = 1
+print(bool(y))
+z = ""
+print(bool(z))
+p = "python"
+print(bool(p))
