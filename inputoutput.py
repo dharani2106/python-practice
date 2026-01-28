9fe52eae1e391c2179fd93c82771375e6ea80983
N.Dharaneswari <dharaneswari2004@gmail.com>
2026-01-28 13:48:41 +0530

    Create inputoutput.py

---
+++ inputoutput.py
@@ -0,0 +1,28 @@
+# topic : inputs and outputs related codes
+x = int(input("enter the 1st number:"))
+y = int(input("enter the 2nd number:"))
+sum = x+y
+print("sum =",sum)
+num = int(input("enter the number:"))
+square = num*num
+cube = num*num*num
+print("square:", square)
+print("cube:", cube)
+name = "dharani"
+print("welcome",name)
+length = int(input("enter length of rectangle:"))
+breadth = int(input("enter breadth of rectangle:"))
+area_of_rectangle = length*breadth
+print("area_of_rectangle:",area_of_rectangle)
+mat = 23
+sci = 31
+soc = 39
+total = mat+sci+soc
+print("total:",total)
+avg = total/3
+print("avg:",avg)
+x = input("enter value:")
+print(type(x))
+num = int(input("enter a number:"))
+res = num + 10
+print("res:",res)
